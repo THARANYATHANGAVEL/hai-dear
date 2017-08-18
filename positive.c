fa8da58bde39aa016d7ffe4b918c2f7de6770ff6
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2017-08-18 18:56:25 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d,&a);
+if(a>0)
+{
+printf("%d is positive",a);
+}
+else if(a==0)
+{
+printf("%d is zero",a);
+else
+{
+prinf("%d is negative",a);
+}
+getch();
+}
