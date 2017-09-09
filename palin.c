df295ab7cb7d2250f5f9a1aa64fb1715c0ff65d9
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2017-09-09 18:50:43 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r=0,remain,p;
+printf("enter n value");
+scanf("%d",&n);
+p=n;
+while(n!=0)
+{
+remain=n%10;
+r=r*10+remain;
+n=n/10;
+}
+if(p==r)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}
+getch();
+}
