be3348577726952eb69b41a8b7a5d3fe7be90d31
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2017-09-17 15:17:51 +0530

    Create stringint.c

---
+++ stringint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+void main() {
+   int n;
+   char m[3];
+ 
+   printf("Enter the m string: ");
+   scanf("%s", m);
+ 
+   num = atoi(m);
+   printf("\nM : %d", n);
+ 
+   getch();
+}
