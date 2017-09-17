94b95b4c8a0b6b4fdb2b77971687f151ef2fd5b4
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2017-09-17 17:05:08 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n, i;
+
+    printf("Enter n value: ");
+    scanf("%d",&n);
+
+    for(i=1; i<=10; ++i)
+    {
+        printf("%d * %d = %d \n", n, i, n*i);
+    }
+    getch();
+}
