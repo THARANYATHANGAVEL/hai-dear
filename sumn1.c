e324f91422edf237522995db3843c5790458b56a
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2017-10-05 19:52:36 +0530

    Update sumn1.c

--- sumn1.c
+++ sumn1.c
@@ -2,10 +2,10 @@
 #include<conio.h>
 void main()
 {
-int n,i,sum=0;
+int a,i,sum=0;
 printf("enter n value");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+scanf("%d",&a);
+for(i=1;i<=a;i++)
 {
 sum=sum+i;
 }
