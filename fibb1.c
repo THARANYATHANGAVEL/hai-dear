0d2eee41bfbc2f545293c5a6ea670bc1f9ab5ddf
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-01-19 16:35:17 +0530

    Create fibb1.c

---
+++ fibb1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+void  main()
+{
+    int i, n, x= 0, y= 1, t;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%d", x);
+         t= x+ y;
+        x= y;
+        y= t;
+    }
+    getch();
+    }
