e1a8932f16871d0b10c4315087a9c05844a9d6a1
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-01-19 19:32:27 +0530

    Create stringrepeat.c

---
+++ stringrepeat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,n;
+    char a[i];
+    scanf("%s",&a);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    printf("%S",a);
+    }
+    return 0;
+}
