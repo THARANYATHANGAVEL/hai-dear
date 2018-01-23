639fd7020b2cab4ae12c5f29e970ed24a70fa0c4
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-01-23 19:41:02 +0530

    Create prgm61.c

---
+++ prgm61.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+char a[100];
+int i,k;
+printf("enter the string:\n");
+scanf("%s",&a);
+printf("how character u want print\n");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("%s",a[i]);
+}
+getch();
+}
