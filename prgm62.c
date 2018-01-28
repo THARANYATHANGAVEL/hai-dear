06b687ac7d5b8a2b9c531e55f08dce10e253be17
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-01-28 18:10:11 +0530

    Create prgm62.c

---
+++ prgm62.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[50];
+int n,c;
+gets(str);
+n=strlen(str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]=='0'||str[i]=='1')
+{
+c++
+}
+}
+if(n==c)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+
+
