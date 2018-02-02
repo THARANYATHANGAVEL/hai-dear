c9309292d4df40cbe2a7432e033cdaa6ae5c8dff
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-02-02 16:10:59 +0530

    Create prgm72.c

---
+++ prgm72.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+  char a[40];
+  int i,cnt=0;
+  scanf("%s",&a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+    {
+      cnt++;
+    }
+  }
+  if(cnt!=0)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+    }
+}
