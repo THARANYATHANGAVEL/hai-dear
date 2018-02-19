4b655bdbfc5a7cb01f592eb963a96f4d574b6ba1
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-02-19 16:11:43 +0530

    Update prgm98.c

--- prgm98.c
+++ prgm98.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 void main()
 {
-    int a[50],n,i;
+    int b[50],n,i;
     printf("enter the array size:");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&b[i]);
         
     }
     for(i=0;i<n;i++)
  {
-    if(a[i]>a[i+1])
+    if(b[i]>b[i+1])
     {
-        printf("%d",--a[i+1]);
+        printf("%d",--b[i+1]);
         break;
     }
  }
