a9a75c5b2557f105184ea5810e3d8038dcb2340e
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-02-21 16:47:17 +0530

    Create 2min.c

---
+++ 2min.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n,a[15],temp;
+    printf("Enter the array size\t");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d",a[1]);
+
+
+}
