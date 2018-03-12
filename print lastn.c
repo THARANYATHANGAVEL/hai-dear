7ed2ee1a9d38732b3e0579b43992d0b533a5b9af
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-03-12 15:48:04 +0530

    Create print lastn.c

---
+++ print lastn.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char a[10];
+  int l=0,i,n,k=0;
+  printf("Enter the string:");
+  scanf("%s",a);
+  printf("Enter the number:");
+  scanf("%d",&n);
+  l=strlen(a);
+  for(i=l-1;i>0;i--)
+  {
+      k++;
+      if(k<=n)
+      {
+    printf("%c",a[i]);
+  }
+  }
+  return 0;
+}
