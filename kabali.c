698b3c68a354b5e1d686ee09ae5a327441371993
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-03-14 15:45:43 +0530

    Create kabali.c

---
+++ kabali.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a[20],b[20],i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&a[i],&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(b[i]>a[i])
+        {
+            printf("%d\n",b[i]-a[i]);
+        }
+    }
+}
