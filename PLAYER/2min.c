ebcedab198ce6f851f3f5e716184760360738d17
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-03-19 19:14:05 +0530

    Create 2min.c

---
+++ PLAYER/2min.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n,a[15],t;
+    printf("Enter the array size:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    printf("%d",a[1]);
+
+
+}
