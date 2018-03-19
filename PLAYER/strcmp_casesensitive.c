3a7e00cc8aac56c06036ec9564cf852d46741ff2
THARANYATHANGAVEL <30930375+THARANYATHANGAVEL@users.noreply.github.com>
2018-03-19 19:35:42 +0530

    Create strcmp_casesensitive.c

---
+++ PLAYER/strcmp_casesensitive.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[50],b[50];
+    int i,c=0,m,n;
+    scanf("%s%s",a,b);
+    m=strlen(a);
+    n=strlen(b);
+    if(m==n)
+    {
+    for(i=0;a[i]!='\0'&&b[i]!='\0';i++)
+    {
+    if(a[i]==b[i])
+    {
+        c++;
+    }
+    }
+    }
+    if(c==m)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
